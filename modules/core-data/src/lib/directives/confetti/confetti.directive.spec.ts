4e713bc02d768ed15e8385025e3e82554bf85eca
Faizan Ali Bugti <faizanalibugti@outlook.com>
2023-09-17 17:46:26 +0500

    test(core-data): Confetti directive

--- modules/core-data/src/lib/directives/confetti/confetti.directive.spec.ts
+++ modules/core-data/src/lib/directives/confetti/confetti.directive.spec.ts
@@ -1,18 +1,12 @@
-import { ElementRef, Renderer2 } from '@angular/core';
+import { ElementRef } from '@angular/core';
 import { ConfettiDirective } from './confetti.directive';
 
 describe('ConfettiDirective', () => {
   let elementRef: ElementRef;
   let directive: ConfettiDirective;
-  let mockRenderer: Renderer2;
 
   beforeEach(() => {
-    mockRenderer = {
-      createElement: jest.fn(),
-      appendChild: jest.fn(),
-    } as unknown as Renderer2;
-
-    directive = new ConfettiDirective(elementRef, mockRenderer);
+    directive = new ConfettiDirective(elementRef);
   });
 
   it('should create an instance', () => {
